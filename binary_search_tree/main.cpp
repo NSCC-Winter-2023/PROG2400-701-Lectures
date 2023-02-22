b05941e31d30cddb3065d9c692b89a047bd78a23
Brian Shewan <brian.shewan@nscc.ca>
2023-02-22 12:12:47 -0400

    added binary search tree demo

---
+++ binary_search_tree/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <iomanip>
+
+struct Node;
+
+using NodePtr = Node*;
+
+struct Node {
+    int m_data {-1};
+    NodePtr m_left {nullptr};
+    NodePtr m_right {nullptr};
+};
+
+class BST {
+private:
+    NodePtr m_root {nullptr};
+
+public:
+    void insert(int num) {
+        insert(num, m_root);
+    }
+
+    friend std::ostream& operator<<(std::ostream& output, BST& bst) {
+        bst.print_tree(output, bst.m_root, 0);
+        return output;
+    }
+
+private:
+    void insert(int num, NodePtr& node) {
+        if (node == nullptr) {
+            node = new Node();
+            node->m_data = num;
+        } else if (num < node->m_data) {
+            // go left
+            insert(num, node->m_left);
+        } else if (num > node->m_data) {
+            // go right
+            insert(num, node->m_right);
+        } else {
+            // duplicate value
+            std::cout << "Duplicate: " << num << std::endl;
+        }
+    }
+
+    void print_tree(std::ostream& output, NodePtr& node, int indent) {
+        if (node != nullptr) {
+            print_tree(output, node->m_right, indent + 8);
+            output << std::setw(indent) << node->m_data << std::endl;
+            print_tree(output, node->m_left, indent + 8);
+        }
+    }
+};
+
+int main() {
+
+    BST bst;
+
+    // test 1 - add nodes to the tree
+    std::cout << "Test 1 - adding nodes" << std::endl;
+    std::cout << "---------------------" << std::endl;
+
+    bst.insert(5);
+    bst.insert(3);
+    bst.insert(7);
+    bst.insert(2);
+    bst.insert(4);
+    bst.insert(6);
+    bst.insert(8);
+
+    std::cout << bst << std::endl;
+
+    return 0;
+}
